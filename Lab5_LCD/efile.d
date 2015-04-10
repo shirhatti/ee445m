31d1bd77b7c00f0a20201cb7b8ab12e42833dc9e
Nelson Wu <ntw248@utexas.edu>
2015-04-10 15:29:39 -0500

    Clean Up Lab5_LCD

--- Lab5_LCD/efile.d
+++
@@ -1,7 +0,0 @@
-.\efile.o: efile.c
-.\efile.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\efile.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
-.\efile.o: ST7735.h
-.\efile.o: diskio.h
-.\efile.o: integer.h
-.\efile.o: efile.h
--- Lab5_LCD/sdctestmain.d
+++
@@ -1,8 +0,0 @@
-.\sdctestmain.o: SDCTestMain.c
-.\sdctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\sdctestmain.o: ..//inc/tm4c123gh6pm.h
-.\sdctestmain.o: PLL.h
-.\sdctestmain.o: diskio.h
-.\sdctestmain.o: integer.h
-.\sdctestmain.o: ST7735.h
-.\sdctestmain.o: efile.h
