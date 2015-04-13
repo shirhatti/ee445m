cf553660d82cd74131c804e837247d31d2d502cd
Nelson Wu <ntw248@utexas.edu>
2015-04-13 14:59:00 -0500

    Sonar Sensor Works

---
+++ InputCapture_4C123/InputCapture.c
@@ -0,0 +1,152 @@
+// InputCapture.c
+// Runs on LM4F120/TM4C123
+// Use Timer0A in edge time mode to request interrupts on the rising
+// edge of PB0 (CCP0), and count the pulses.
+// Daniel Valvano
+// September 11, 2013
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
+   Example 6.1, Program 6.1
+
+ Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// external signal connected to PB6 (T0CCP0) (trigger on rising edge)
+#include <stdint.h>
+#include "inc/tm4c123gh6pm.h"
+#include "InputCapture.h"
+
+#define NVIC_EN0_INT19          0x00080000  // Interrupt 19 enable
+
+#define TIMER_CFG_16_BIT        0x00000004  // 16-bit timer configuration,
+                                            // function is controlled by bits
+                                            // 1:0 of GPTMTAMR and GPTMTBMR
+#define TIMER_TAMR_TACMR        0x00000004  // GPTM TimerA Capture Mode
+#define TIMER_TAMR_TAMR_CAP     0x00000003  // Capture mode
+#define TIMER_CTL_TAEN          0x00000001  // GPTM TimerA Enable
+#define TIMER_CTL_TAEVENT_POS   0x00000000  // Positive edge
+#define TIMER_IMR_CAEIM         0x00000004  // GPTM CaptureA Event Interrupt
+                                            // Mask
+#define TIMER_ICR_CAECINT       0x00000004  // GPTM CaptureA Event Interrupt
+                                            // Clear
+#define TIMER_TAILR_M           0xFFFFFFFF  // GPTM Timer A Interval Load
+                                            // Register
+																						
+#define NVIC_EN2_INT70					0x00000040	
+#define TIMER_CFG_32_BIT_TIMER  0x00000000  // 32-bit timer configuration
+#define TIMER_TAMR_TACDIR       0x00000010  // GPTM Timer A Count Direction
+#define TIMER_TAMR_TAMR_PERIOD  0x00000002  // Periodic Timer mode
+#define TIMER_IMR_TATOIM        0x00000001  // GPTM TimerA Time-Out Interrupt
+                                            // Mask
+#define TIMER_ICR_TATOCINT      0x00000001  // GPTM TimerA Time-Out Raw
+                                            // Interrupt
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+void (*PeriodicTask)(void);   // user function
+
+//------------TimerCapture_Init------------
+// Initialize Timer0A in edge time mode to request interrupts on
+// the rising edge of PB0 (CCP0).  The interrupt service routine
+// acknowledges the interrupt and calls a user function.
+// Input: task is a pointer to a user function
+// Output: none
+void TimerCapture_Init(void(*task)(void)){long sr;
+  sr = StartCritical();
+  SYSCTL_RCGCTIMER_R |= 0x01;// activate timer0
+  SYSCTL_RCGCGPIO_R |= 0x02; // activate port B
+  while((SYSCTL_PRGPIO_R&0x0002) == 0){};// ready?
+
+  PeriodicTask = task;             // user function 
+  GPIO_PORTB_DIR_R &= ~0x40;       // make PB6 in
+  GPIO_PORTB_AFSEL_R |= 0x40;      // enable alt funct on PB6
+  GPIO_PORTB_DEN_R |= 0x40;        // enable digital I/O on PB6
+                                   // configure PB6 as T0CCP0
+  GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R&0xF0FFFFFF)+0x07000000;
+  GPIO_PORTB_AMSEL_R &= ~0x40;     // disable analog functionality on PB6
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = TIMER_CFG_16_BIT; // configure for 16-bit timer mode
+                                   // configure for capture mode, default down-count settings
+  TIMER0_TAMR_R = (TIMER_TAMR_TACMR|TIMER_TAMR_TAMR_CAP);
+                                   // configure for falling edge event
+  TIMER0_CTL_R &= ~(TIMER_CTL_TAEVENT_NEG|0xC);
+  TIMER0_TAILR_R = TIMER_TAILR_M;  // max start value
+  TIMER0_IMR_R |= TIMER_IMR_CAEIM; // enable capture match interrupt
+  TIMER0_ICR_R = TIMER_ICR_CAECINT;// clear timer0A capture match flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 16-b, +edge timing, interrupts
+                                   // Timer0A=priority 2
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
+  NVIC_EN0_R = NVIC_EN0_INT19;     // enable interrupt 19 in NVIC
+  EndCritical(sr);
+}
+
+void Timer0A_Handler(void){
+  TIMER0_ICR_R = TIMER_ICR_CAECINT;// acknowledge timer0A capture match
+  (*PeriodicTask)();               // execute user task
+}
+
+void Init_Timer4A(void) {
+	long sr;
+	volatile unsigned long delay;
+	
+	sr = StartCritical();
+  SYSCTL_RCGCTIMER_R |= 0x10;
+	
+  delay = SYSCTL_RCGCTIMER_R;
+	delay = SYSCTL_RCGCTIMER_R;
+	
+  TIMER4_CTL_R &= ~TIMER_CTL_TAEN; // 1) disable timer1A during setup
+                                   // 2) configure for 32-bit timer mode
+  TIMER4_CFG_R = TIMER_CFG_32_BIT_TIMER;
+                                   // 3) configure for periodic mode, default down-count settings
+  TIMER4_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER4_TAILR_R = 0xFFFFFFFF - 1;     // 4) reload value
+                                   // 5) clear timer1A timeout flag
+  TIMER4_ICR_R = TIMER_ICR_TATOCINT;
+  TIMER4_IMR_R |= TIMER_IMR_TATOIM;// 6) arm timeout interrupt
+								   // 7) priority shifted to bits 15-13 for timer1A
+  NVIC_PRI17_R = (NVIC_PRI17_R&0xFF00FFFF)|(1 << 21);	//3
+  NVIC_EN2_R = NVIC_EN2_INT70;     // 8) enable interrupt 21 in NVIC
+  TIMER4_TAPR_R = 0;
+  TIMER4_CTL_R |= TIMER_CTL_TAEN;  // 9) enable timer1A
+	
+  EndCritical(sr);
+}
+
+void Timer4A_Handler(void){ 
+  TIMER4_ICR_R = TIMER_ICR_TATOCINT;// acknowledge timer1A timeout
+}
+
+// ******** OS_Time ************
+// return the system time 
+// Inputs:  none
+// Outputs: time in 12.5ns units, 0 to 4294967295
+// The time resolution should be less than or equal to 1us, and the precision 32 bits
+// It is ok to change the resolution and precision of this function as long as 
+//   this function and OS_TimeDifference have the same resolution and precision 
+unsigned long OS_Time(void) { 
+	return TIMER4_TAILR_R - TIMER4_TAV_R;
+}
+
+void Timer4A_Wait(unsigned long delay){
+  volatile unsigned long elapsedTime;
+  unsigned long startTime = TIMER4_TAV_R;
+  do {
+    elapsedTime = startTime-TIMER4_TAV_R;
+  }
+  while(elapsedTime <= delay);
+}
---
+++ InputCapture_4C123/InputCaptureTestMain.c
@@ -0,0 +1,143 @@
+// InputCaptureTestMain.c
+// Runs on LM4F120/TM4C123
+// Use Timer0A in edge time mode to request interrupts on the rising
+// edge of PB0 (CCP0), and count the pulses.
+// Daniel Valvano
+// September 11, 2013
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2014
+   Example 6.1, Program 6.1
+
+ Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// external signal connected to PB6 (T0CCP0) (trigger on rising edge)
+#include <stdint.h>
+#include "inc/tm4c123gh6pm.h"
+#include "InputCapture.h"
+#include "PLL.h"
+
+#define PB6  (*((volatile unsigned long *)0x40005100))
+	
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+
+volatile uint32_t Count;      // incremented on interrupt
+void UserTask(void){
+  GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R^0x04; // toggle PF2
+  Count = Count + 1;
+}
+
+static int counter;
+void Init_Timer2A(uint32_t period) {
+	long sr;
+	volatile unsigned long delay;
+	
+	sr = StartCritical();
+	counter = 0;
+  SYSCTL_RCGCTIMER_R |= 0x04;
+	
+  delay = SYSCTL_RCGCTIMER_R;
+	delay = SYSCTL_RCGCTIMER_R;
+	
+  TIMER2_CTL_R &= ~TIMER_CTL_TAEN; // 1) disable timer1A during setup
+                                   // 2) configure for 32-bit timer mode
+  TIMER2_CFG_R = TIMER_CFG_32_BIT_TIMER;
+                                   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER2_TAILR_R = period - 1;     // 4) reload value
+                                   // 5) clear timer1A timeout flag
+  TIMER2_ICR_R = TIMER_ICR_TATOCINT;
+  TIMER2_IMR_R |= TIMER_IMR_TATOIM;// 6) arm timeout interrupt
+								   // 7) priority shifted to bits 31-29 for timer2A
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|(1 << 29);	
+  NVIC_EN0_R = NVIC_EN0_INT23;     // 8) enable interrupt 23 in NVIC
+  TIMER2_TAPR_R = 0;
+  TIMER2_CTL_R |= TIMER_CTL_TAEN;  // 9) enable timer2A
+	
+  EndCritical(sr);
+}
+
+void Timer2A_Handler(void){ 
+	TIMER2_ICR_R = TIMER_ICR_TATOCINT;// acknowledge timer2A timeout
+	
+	if(counter == 100)
+	{
+		counter = 1;
+		
+		GPIO_PORTB_AFSEL_R &= ~0x40; // regular port function
+		GPIO_PORTB_DIR_R |= 0x40;    // make PD3-0 out
+		PB6 = 0x00;
+		PB6 = 0x40;
+		Timer4A_Wait(800);	// 10 us
+		PB6 = 0x40;
+		TIMER0_TAV_R = 0;
+		GPIO_PORTB_DIR_R &= ~0x40;       // make PB6 in
+		GPIO_PORTB_AFSEL_R |= 0x40;      // enable alt funct on PB6
+	}
+	else {
+		counter++;
+	}
+}
+
+int main(void){
+	PLL_Init();
+                                   // activate port F
+  SYSCTL_RCGCGPIO_R |= 0x20;
+  while((SYSCTL_PRGPIO_R&0x0020) == 0){};// ready?
+  Count = 0;                       // allow time to finish activating
+  GPIO_PORTF_DIR_R |= 0x04;        // make PF2 out (built-in LED)
+  GPIO_PORTF_AFSEL_R &= ~0x04;     // disable alt funct on PF2
+  GPIO_PORTF_DEN_R |= 0x04;        // enable digital I/O on PF2
+                                   // configure PF2 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF0FF)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;          // disable analog functionality on PF
+		
+	DisableInterrupts();
+	Init_Timer4A();
+	Init_Timer2A(80000);
+
+#ifdef DEBUG		
+	SYSCTL_RCGCGPIO_R |= 0x02; // activate port B
+  while((SYSCTL_PRGPIO_R&0x0002) == 0){};// ready?
+		
+	GPIO_PORTB_DIR_R |= 0x40;    // make PE3-0 output heartbeats
+  GPIO_PORTB_AFSEL_R &= ~0x40;   // disable alt funct on PE3-0
+  GPIO_PORTB_DEN_R |= 0x40;     // enable digital I/O on PE3-0
+  GPIO_PORTB_PCTL_R = ~0x0000FFFF;
+  GPIO_PORTB_AMSEL_R &= ~0x40;;      // disable analog functionality on PF	
+		
+	PB6 = 0x00;
+	PB6 = 0x40;
+	Timer4A_Wait(80000);
+	PB6 = 0x00;
+	
+	GPIO_PORTB_DIR_R &= ~0x40;       // make PB6 in
+  GPIO_PORTB_AFSEL_R |= 0x40;      // enable alt funct on PB6
+  GPIO_PORTB_DEN_R |= 0x40;        // enable digital I/O on PB6
+                                   // configure PB6 as T0CCP0
+  GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R&0xF0FFFFFF)+0x07000000;
+  GPIO_PORTB_AMSEL_R &= ~0x40;     // disable analog functionality on PB6
+#endif
+	
+	TimerCapture_Init(UserTask);
+	EnableInterrupts();
+	
+  while(1){
+    WaitForInterrupt();
+  }
+}
